2741f478b08384e6d5ccb5a92e0a36a2653ae4fd
Meynard Duran <pvt_mduran18@yahoo.com>
2022-07-13 21:32:52 -0700

    homework complete

---
+++ javascriptAssignment2/index.js
@@ -0,0 +1,38 @@
+const state = {
+   a: 'apple',
+   b: 'banana',
+   c: 'city',
+   d: 'dom',
+   e: 'end',
+   f: 'forEach',
+   g: 'gesdrwesf',
+};
+
+const body = document.getElementById('body');
+const select = document.createElement('select');
+const select2 = document.createElement('select');
+body.append(select, select2);
+
+const handleOption = (e) => {
+   alert('hi');
+};
+Object.keys(state).map((item) => {
+   const option = document.createElement('option');
+   select.append(option);
+   option.append(item);
+   option.value = item;
+});
+
+Object.values(state).map((item) => {
+   const option = document.createElement('option');
+   select2.append(option);
+   option.append(item);
+   option.value = item;
+});
+
+function selectedItem(e) {
+   select.selectedIndex = e.target.selectedIndex;
+   select2.selectedIndex = e.target.selectedIndex;
+}
+select.addEventListener('change', selectedItem);
+select2.addEventListener('change', selectedItem);
